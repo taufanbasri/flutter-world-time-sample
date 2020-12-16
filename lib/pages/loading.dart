8c6351e74994739ffbf311d6448246d66e9cbfaa
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-16 07:27:08 +0700

    WorldTime Custom Class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,34 +8,32 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
+  String time = 'loading';
 
-    // make the request
-    Response response = await get('https://worldtimeapi.org/api/timezone/Asia/Jakarta');
-    Map data = jsonDecode(response.body);
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Jakarta', flag: 'indonesia.jpg', url: 'Asia/Jakarta');
 
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+    await instance.getTime();
 
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
-
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
 
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      )
     );
   }
 }
