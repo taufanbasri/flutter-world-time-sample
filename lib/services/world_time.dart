e2c48bffb4945719fb5d1e30f72da50aa2177113
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-16 08:55:05 +0700

    Error Handling

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,20 +11,27 @@ class WorldTime {
   WorldTime({ this.location, this.flag, this.url });
 
   Future<void> getTime() async {
-    // make the request
-    Response response = await get('https://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
 
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+    try {
+      // make the request
+      Response response = await get('https://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
 
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
 
-    // set the time property
-    time = now.toString();
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      // set the time property
+      time = now.toString();
+    } catch(e) {
+      print('caught error: $e');
+
+      time = 'could not get time data';
+    }
 
   }
 
